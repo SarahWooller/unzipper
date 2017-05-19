6895d0234ab89080a3d8b69feea739834b25f47b
Sarah Wooller <sarahwooller@Sarahs-MBP.home>
2017-05-19 20:59:59 +0100

    third time lucky

--- unzipper/__init__.py
+++ unzipper/__init__.py
@@ -11,22 +11,27 @@ Can be used from command line"""
 
 directory = input('name of directory with files in')
 
-zipper = r'(.*).zip'
-for name in os.listdir(directory):
-    try:
-        #extract the student id
-        student_id = re.match(zipper,name).group(1)+'/'
-        #make a directory named student id
-        p=subprocess.Popen(['mkdir', directory+student_id])
-        #wait to finish that before proceeding
-        p.communicate()
-        #move the file to the new directory
-        p=subprocess.Popen(['mv',directory+name,directory+student_id+name])
-        #wait again
-        p.communicate()
-        #extract all the information
-        with zipfile.ZipFile(directory+student_id+name, "r") as z:
-            z.extractall(directory+student_id)
-    #don't do anything with none zip files (which throw an AttributeError for regex)
-    except AttributeError:
-        pass
+def main():
+    zipper = r'(.*).zip'
+    for name in os.listdir(directory):
+        try:
+            #extract the student id
+            student_id = re.match(zipper,name).group(1)+'/'
+            #make a directory named student id
+            p=subprocess.Popen(['mkdir', directory+student_id])
+            #wait to finish that before proceeding
+            p.communicate()
+            #move the file to the new directory
+            p=subprocess.Popen(['mv',directory+name,directory+student_id+name])
+            #wait again
+            p.communicate()
+            #extract all the information
+            with zipfile.ZipFile(directory+student_id+name, "r") as z:
+                z.extractall(directory+student_id)
+        #don't do anything with none zip files (which throw an AttributeError for regex)
+        except AttributeError:
+            pass
+
+
+if "__name__"=="main":
+    main()
